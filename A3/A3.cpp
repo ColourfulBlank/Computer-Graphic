f514ce794255298d5639f5588873de753561b004
ColourfulBlank <w257wang@uwaterloo.ca>
2016-06-13 17:46:10 -0400

    shit

--- A3/A3.cpp
+++ A3/A3.cpp
@@ -668,15 +668,16 @@ bool A3::mouseButtonInputEvent (
 				// picking = true;
 			pickingMode(1);
 			glClear(GL_COLOR_BUFFER_BIT);
-			picking_xPos = last_xPos;
-			picking_xPos = last_yPos;
+			// picking_xPos = last_xPos;
+			// picking_xPos = last_yPos;
+			renderSceneGraph(*m_rootNode);
 			glReadPixels(last_xPos, m_windowHeight - last_yPos, 1, 1, GL_RGB, GL_FLOAT, &picked_colour);
 			picked_Id[lookingUpId(vec3(picked_colour[0], picked_colour[1], picked_colour[2]))] = picked_Id[lookingUpId(vec3(picked_colour[0], picked_colour[1], picked_colour[2]))] == 1 ? 0 : 1;
 			cout << picked_colour[0] << " " << picked_colour[1] << " " << picked_Id[2] << endl;
 			cout << lookingUpId(vec3(picked_colour[0], picked_colour[1], picked_colour[2])) << endl;
 			// }
 			pickingMode(0);
-			// glClear(GL_COLOR_BUFFER_BIT);
+			glClear(GL_COLOR_BUFFER_BIT);
 		}	
 	}
 	if (mouseState[0] == 0){
