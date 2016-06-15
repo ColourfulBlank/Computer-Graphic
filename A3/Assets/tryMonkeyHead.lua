27b1b8b407097bf80e211a185520799d372e1cd4
ColourfulBlank <w257wang@uwaterloo.ca>
2016-06-15 03:40:31 -0400

    shit

--- A3/Assets/tryMonkeyHead.lua
+++ A3/Assets/tryMonkeyHead.lua
@@ -155,11 +155,11 @@ rightHipTorsoConnection:scale(0.05, 0.05, 0.05)
 rightHipTorsoConnection:translate(0.25, -0.3, 0.0)
 rightHipTorsoConnection:set_material(skin)
 
-leftHipTorsoConnectionJoint = gr.joint('leftHipTorsoConnectionJoint', {-45, 0, 45}, {-10, 0, 10})
+leftHipTorsoConnectionJoint = gr.joint('leftHipTorsoConnectionJoint', {-10, 0, 10}, {-10, 0, 10})
 leftHipTorsoConnectionJoint:translate(0.0, 0.0, 0.0)
 leftHipTorsoConnection:add_child(leftHipTorsoConnectionJoint)
 
-rightHipTorsoConnectionJoint = gr.joint('rightHipTorsoConnectionJoint', {-45, 0, 45}, {-10, 0, 10})
+rightHipTorsoConnectionJoint = gr.joint('rightHipTorsoConnectionJoint', {-10, 0, 10}, {-10, 0, 10})
 rightHipTorsoConnectionJoint:translate(0.0, 0.0, 0.0)
 rightHipTorsoConnection:add_child(rightHipTorsoConnectionJoint)
 
